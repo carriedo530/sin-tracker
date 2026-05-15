4bd0c9ee5de2a527c042929329fc09b474a287d8
RikkaTindle <rikka@firstwavefund.com>
2026-05-15 16:53:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -276,7 +276,6 @@ def load_submissions_from_sheet():
         return None
     df = pd.DataFrame(records)
     df["Submission Time"] = pd.to_datetime(df["Submission Time"], utc=True, errors="coerce")
-    df["Submission Date"] = df["Submission Time"].dt.tz_convert("America/New_York").dt.strftime("%Y-%m-%d")
     st.session_state.submissions_cache = df
     return df
 
